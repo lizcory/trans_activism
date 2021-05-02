c22b353d79734f723b26c6f82d65f51a5588d93d
lizcory <evcory96@gmail.com>
2021-05-02 01:39:38 -0400

    interactive + box style + text

--- map.js
+++ map.js
@@ -1,28 +1,20 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZXZjb3J5IiwiYSI6ImNrZzhuZDUzeTBjZ2oyc3BjbmE1cmZrbnkifQ.lQXtozEwTyP33-8zN1hi_A';
 
-var map = new mapboxgl.Map({
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZXZjb3J5IiwiYSI6ImNrZzhuZDUzeTBjZ2oyc3BjbmE1cmZrbnkifQ.lQXtozEwTyP33-8zN1hi_A';
+  var map = new mapboxgl.Map({
     container: 'map',
     // style: 'mapbox://styles/evcory/ckmqruueh1e3c17pq0nifltu2',
+    // center: [-98.131278,39.820130], // starting position
+    // zoom: "3.25" // starting zoom
+
+
     style: 'mapbox://styles/evcory/cko6k9kdl2m9517kq8z3pah9g',
-    center: [-98.131278,39.820130], // starting position
-    zoom: "3.25" // starting zoom
+    center: [-96.131278,39.820130], // starting position
+    zoom: "2.8" // starting zoom
   });
 
 
-map.addControl(new mapboxgl.NavigationControl());
+  map.addControl(new mapboxgl.NavigationControl());
 
-map.addControl(L.mapbox.legendControl(legend));
-
-map.legendControl.addLegend(document.getElementById('legend').innerHTML)
-
-
-// // cluster GeoJSON points
-// var index = supercluster({radius: 40, maxZoom: 16}).load(geojson.features);
-// // get GeoJSON clusters given a bounding box and zoom
-// var clusters = index.getClusters([-180, -85, 180, 85], 2);
-// // get a JSON vector tile in the same format as GeoJSON-VT
-// var tile = index.getTile(7, 523, 125);
-  
 
   // popups
   map.on('click', function(e) {
@@ -34,24 +26,58 @@ map.legendControl.addLegend(document.getElementById('legend').innerHTML)
       return;
     }
 
-
-    var feature = features[0];
-
-    console.log(feature);
-
+    let feature = features[0];
 
     var popup = new mapboxgl.Popup({ offset: [0, -15] })
       .setLngLat(feature.geometry.coordinates)
       .setHTML(
-        // '<h3>' + 
-        // feature.properties.tweet + 
-        // '</h3><p>' + 
-        '<p>' + 
-        feature.properties.created_at + 
-        '</p><p>' + 
-        feature.properties.place_info_full_name + 
-        '</p>')
-      // .setLngLat(feature.geometry.coordinates)
+      // '<h3>' + 
+      // feature.properties.X + 
+      // '</h3><p>' + 
+      '<h3>' +
+      feature.properties.place_info_full_name + 
+      '</h3><p>' + 
+      'Tweet date: ' +
+      '<br>' +
+      feature.properties.created_at + 
+      '</p>')
       .addTo(map);
 
+});
+
+
+// Change the cursor to a pointer when the mouse is over the states layer.
+map.on('mouseenter', 'transrightsarehumanrights-15000', function () {
+  map.getCanvas().style.cursor = 'pointer';
   });
+   
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'transrightsarehumanrights-15000', function () {
+  map.getCanvas().style.cursor = '';
+  });
+
+
+// map.on('click', function(e) {
+//   var features = map.queryRenderedFeatures(e, {
+//     layers: ['geo-json-states-bills'] // replace this with the name of the layer
+//   });
+
+//   if (!features.length) {
+//     return;
+//   }
+
+//   let feature = features[0];
+
+//   var popup = new mapboxgl.Popup({ offset: [0, -15] })
+//     .setLngLat(feature.geometry.coordinates)
+//     .setHTML(
+//     // '<h3>' + 
+//     // feature.properties.X + 
+//     // '</h3><p>' + 
+//     '<p>' + 
+//     feature.properties.bills + 
+//     '</p>')
+//     .addTo(map)
+
+//   });
+
